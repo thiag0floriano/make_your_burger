2ccc73f1a283c79e916b0867a2666ca627ce67b3
Thiago Floriano <110630909+thiag0floriano@users.noreply.github.com>
2024-01-17 14:43:34 -0300

    make v1.1

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav>
+        <RouterLink to="/">
+            <img src="" id="logo">
+        </RouterLink>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/pedidos">Pedidos</router-link>
+    </nav>
+</template>
+
+<script>
+export default{
+    name: 'Navbar',
+    props: ["logo", "alt"]
+}
+</script>
+
+<style scoped>
+
+</style>

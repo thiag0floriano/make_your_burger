266eb2cf3dfa1daa38a6f576344be7986fed81b6
Thiago Floriano <110630909+thiag0floriano@users.noreply.github.com>
2024-01-23 17:53:54 -0300

    burgerForm v1

---
+++ src/components/BurgerForm.vue
@@ -0,0 +1,24 @@
+<template>
+    <p>Burger for Component</p>
+    <div class="imput-container">
+        <label for="nome">Nome do cliente:</label>
+        <input type="text" id="nome" name="nome" v-model="nome" placeholder="Digite o seu nome">
+    </div>
+    <div class="imput-container">
+        <label for="pao">Escolha o pão:</label>
+        <select name="pao" id="pao" v-model="pao">
+            <option value="">Selecione o seu pão</option>
+            <option value="Integral">Integral</option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default{
+    name:'BurgerForm'
+}
+</script>
+
+<style scopes>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,18 +2,20 @@
   <Banner />
   <div class="main-container">
     <H1>Monte o seu burger</H1>
-    <p>formulario</p>
+    <BurgerForm />
   </div>
 </template>
 
 <script>
 
 import Banner from '@/components/Banner.vue';
+import BurgerForm from '@/components/BurgerForm.vue';
 
 export default{
   name: 'HomerView',
   components: {
-    Banner
+    Banner,
+    BurgerForm
   }
 }
 </script>
